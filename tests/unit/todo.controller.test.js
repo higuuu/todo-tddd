3654ddc39a018cdaa5c767cd999c08743c438cbd
higuzero <higuzeroggw@gmail.com>
2020-09-04 13:14:51 +0900

    make the file to pass unit test

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,6 +1,6 @@
 const TodoController = require("../../controllers/todo.controller")
 
-descriibe("TodoController.createTodo", ()=>{
+describe("TodoController.createTodo", ()=>{
     it("should have a createTodo function", () => {
         expect(typeof TodoController.createTodo).toBe("function")
     })
